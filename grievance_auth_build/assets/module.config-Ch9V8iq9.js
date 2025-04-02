da4519116afa00b0b731c66dba4a80bfaef186f8
shiven358 <jha.shivendra4@gmail.com>
2025-04-02 18:18:02 +0530

    add grievance auth

---
+++ grievance_auth_build/assets/module.config-Ch9V8iq9.js
@@ -0,0 +1,6 @@
+import{U as c}from"./index-Di_aGywo.js";/**
+ * @license lucide-react v0.428.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const e=c("FileText",[["path",{d:"M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z",key:"1rqfz7"}],["path",{d:"M14 2v4a2 2 0 0 0 2 2h4",key:"tnqrlb"}],["path",{d:"M10 9H8",key:"b1mrlr"}],["path",{d:"M16 13H8",key:"t4e002"}],["path",{d:"M16 17H8",key:"z1uh3a"}]]),s="/images/profile.png",E="66d30fc3244bfdc8fc34c7f8",f="66d30fc8244bfdc8fc34c7fb",t="66d317939285f57005e7c4b5",b="66d5523f7741e5ef86ebc3c8",M="66d57034c965c99dbe57be6e",d="66d5b0f77be6a0da66671852",o="66d5b1a87be6a0da66671876",_="66ec16adc5186d091150d0e2",n="66ec22c112e1a6266fff9882",O="66f10277abec0c518979b0f6",T="66f102a2abec0c518979b0fe",R="66f102bfabec0c518979b106",D="66f102d5abec0c518979b10a",L="66f102e0abec0c518979b10e",U="66f102f3abec0c518979b112";export{e as F,M,E as a,f as b,t as c,b as d,d as e,o as f,_ as g,n as h,O as i,T as j,R as k,D as l,L as m,U as n,s as p};
